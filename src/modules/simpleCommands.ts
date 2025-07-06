bed3466120919622dd46128c57fef7861c7df679
K0bus <contact@k0bus.fr>
2025-07-07 00:46:29 +0200

    Update the info command

--- src/modules/simpleCommands.ts
+++ src/modules/simpleCommands.ts
@@ -1,6 +1,8 @@
 import {
+    ChatInputCommandInteraction,
+    Guild,
     MessageFlags,
-  SlashCommandBuilder,
+    SlashCommandBuilder,
 } from "discord.js";
 
 import { MovieDb } from "moviedb-promise";
@@ -14,8 +16,24 @@ const simpleCommands: BotModule = {
         slashCommand: new SlashCommandBuilder()
             .setName("info")
             .setDescription("Renvoie les informatios du Bot Discord"),
-        async execute(client, interaction) {
-            await interaction.reply({content: "Bot is running well...", flags: MessageFlags.Ephemeral})
+        async execute(client, interaction: ChatInputCommandInteraction) {
+            const members = await interaction.guild!.members.fetch();
+            const humanCount = members.filter(m => !m.user.bot).size;
+
+            await interaction.reply({
+                embeds: [
+                    {
+                        title: "🧩 Information de Koby",
+                        fields: [
+                            { name: "Serveurs", value: `${client.guilds.cache.size}`, inline: true },
+                            { name: "Membre sur ce serveur", value: `${humanCount}`, inline: true },
+                        ],
+                        timestamp: new Date().toISOString(),
+                        color: 0x00aaff,
+                    },
+                ],
+                flags: MessageFlags.Ephemeral
+            })
         }
     },
     {
