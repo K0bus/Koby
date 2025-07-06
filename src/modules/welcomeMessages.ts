254a7f80f7ada474d6339ed1b1dc488bb5a529df
K0bus <contact@k0bus.fr>
2025-07-06 19:38:29 +0200

    Add test command

--- src/modules/welcomeMessages.ts
+++ src/modules/welcomeMessages.ts
@@ -1,6 +1,8 @@
 import {
+  ChatInputCommandInteraction,
+  Client,
   Events,
-  GuildMember, MessageCreateOptions
+  GuildMember, InteractionContextType, MessageCreateOptions, MessageFlags, PermissionFlagsBits, SlashCommandBuilder
 } from "discord.js";
 
 import { BotModule } from "../types/BotTypes";
@@ -17,32 +19,56 @@ export type WelcomeConfig = {
 
 const welcomeMessage: BotModule = {
   name: "welcomeMessage",
-  commands: [],
+  commands: [
+    {
+      slashCommand: new SlashCommandBuilder()
+          .setName("welcometest")
+          .setDescription("Test your Welcome message")
+          .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+          .setContexts(InteractionContextType.Guild),
+      async execute(client: Client, interaction :ChatInputCommandInteraction) {
+        if(interaction.guild != null)
+        {
+          if(interaction.memberPermissions && interaction.memberPermissions.has(PermissionFlagsBits.Administrator))
+          {
+            if(interaction.member)
+            {
+              await sendWelcome(<GuildMember>interaction.member)
+            }
+          }
+        }
+      }
+    }
+  ],
   event: [
     {
       eventType: Events.GuildMemberAdd,
       async execute(client, member: GuildMember) {
-        const welcomeChannel = member.guild.systemChannel;
-        if (welcomeChannel) {
-          const config = ConfigManager.getConfig<WelcomeConfig>(
-              "welcomeMessage",
-              member.guild.id
-          );
-          const channel = member.guild.channels.cache.get(config.channelId);
-          if(channel?.isTextBased())
-          {
-            await channel.send(parseMessage(config.message, member, parseString))
-          }
-        }
+        await sendWelcome(member);
       },
       once: false,
     },
   ],
 };
 
+
+async function sendWelcome(member: GuildMember) {
+  const welcomeChannel = member.guild.systemChannel;
+  if (welcomeChannel) {
+    const config = ConfigManager.getConfig<WelcomeConfig>(
+        "welcomeMessage",
+        member.guild.id
+    );
+    const channel = member.guild.channels.cache.get(config.channelId);
+    if(channel?.isTextBased())
+    {
+      await channel.send(parseMessage(config.message, member, parseString))
+    }
+  }
+}
 function parseString(text: string | undefined, member: GuildMember): string | undefined {
   if(text) {
-    text.replace("%userid%", member.user.id)
+    text = text.replace("%userid%", member.user.id)
   }
   return text;
 }
